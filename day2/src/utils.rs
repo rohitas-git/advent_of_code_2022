3d727e4d1365ffe96f764510e3f1d4b03bfd3756
rohitasb <rohitasbansal@icloud.com>
2023-10-25 18:32:00 +0530

    Adding another solution to day1,2,3 from fasthanli.me

--- day2/src/utils.rs
+++ day2/src/utils.rs
@@ -12,13 +12,13 @@ enum Hand {
     Scissor,
 }
 
-impl Hand{
+impl Hand {
     fn from_u8(val: u8) -> Option<Hand> {
         match val {
-            0 => return Some(Rock),
-            1 => return Some(Paper),
-            2 => return Some(Scissor),
-            _ => return None
+            0 => Some(Rock),
+            1 => Some(Paper),
+            2 => Some(Scissor),
+            _ => None,
         }
     }
 }
@@ -48,19 +48,21 @@ impl Round {
         // If Win else Lose
         if opp == Rock && me == Paper {
             return win;
-        } else if opp == Paper && me == Scissor {
+        }
+        if opp == Paper && me == Scissor {
             return win;
-        } else if opp == Scissor && me == Rock {
-            return win;
+        }
+        if opp == Scissor && me == Rock {
+            win
         } else {
-            return lose;
+            lose
         }
     }
 }
 
 use Hand::*;
 
-pub fn total_score(file_path: &str, read_round: fn(&String)-> Result<Round, &str>) -> u32 {
+pub fn total_score(file_path: &str, read_round: fn(&str) -> Result<Round, &str>) -> u32 {
     let lines = read_lines(file_path);
     let total: u32 = lines
         .iter()
@@ -69,19 +71,18 @@ pub fn total_score(file_path: &str, read_round: fn(&String)-> Result<Round, &str
     total
 }
 
-pub fn read_round(line: &String) -> Result<Round, &str> {
+pub fn read_round(line: &str) -> Result<Round, &str> {
     let content: Vec<&str> = line.split(" ").collect();
     let opponent = parse_move(content[0])?;
     let me = parse_move(content[1])?;
     Ok(Round { opponent, me })
 }
 
-pub fn diff_read_round(line: &String) -> Result<Round, &str> {
+pub fn diff_read_round(line: &str) -> Result<Round, &str> {
     let content: Vec<&str> = line.split(" ").collect();
     let round = diff_parse_move(content)?;
     Ok(round)
 }
-
 
 fn parse_move(choice: &str) -> Result<Hand, &str> {
     match choice {
@@ -93,7 +94,7 @@ fn parse_move(choice: &str) -> Result<Hand, &str> {
 }
 
 // parse move according to the different strategy
-fn diff_parse_move(round: Vec<&str>) -> Result<Round, &str>{
+fn diff_parse_move(round: Vec<&str>) -> Result<Round, &str> {
     let h1 = round[0];
     let h2 = round[1];
 
@@ -102,17 +103,18 @@ fn diff_parse_move(round: Vec<&str>) -> Result<Round, &str>{
         "B" | "Y" => Ok(Hand::Paper),
         "C" | "Z" => Ok(Hand::Scissor),
         _ => Err("Error parsing the move"),
-    }.unwrap();
+    }
+    .unwrap();
 
     let val = opponent.clone() as u8 - 1;
     let me = match h2 {
-        "X" => Hand::from_u8(if val==0{2} else {(val-1)%3}).unwrap()  ,
+        "X" => Hand::from_u8(if val == 0 { 2 } else { (val - 1) % 3 }).unwrap(),
         "Y" => Hand::from_u8(val).unwrap(),
-        "Z" => Hand::from_u8((val+1)%3).unwrap(),
+        "Z" => Hand::from_u8((val + 1) % 3).unwrap(),
         _ => return Err("Error parsing the move"),
     };
 
-    Ok(Round{opponent, me})
+    Ok(Round { opponent, me })
 }
 
 fn read_lines(file_path: &str) -> Vec<String> {
@@ -185,8 +187,279 @@ mod test_utils {
 
         let total = total_score(INPUT, diff_read_round);
         assert_eq!(12, total);
-    
+    }
+}
+
+mod another_solution {
+    use std::convert::TryFrom;
+    use std::str::FromStr;
+
+    #[derive(Debug, Clone, Copy)]
+    enum Move {
+        Rock,
+        Paper,
+        Scissors,
     }
 
+    #[derive(Debug, Clone, Copy)]
+    pub enum Outcome {
+        Win,
+        Draw,
+        Loss,
+    }
 
+    trait MoveTrait {
+        fn inherent_points(self) -> usize;
+        fn beats(self, other: Move) -> bool;
+        fn outcome(self, theirs: Move) -> Outcome;
+    }
+
+    trait OutcomeTrait {
+        fn inherent_points(self) -> usize;
+        fn matching_move(self, theirs: Move) -> Move;
+    }
+
+    impl MoveTrait for Move {
+        fn inherent_points(self) -> usize {
+            match self {
+                Move::Rock => 1,
+                Move::Paper => 2,
+                Move::Scissors => 3,
+            }
+        }
+
+        fn beats(self, other: Move) -> bool {
+            matches!(
+                (self, other),
+                (Self::Rock, Self::Scissors)
+                    | (Self::Paper, Self::Rock)
+                    | (Self::Scissors, Self::Paper)
+            )
+        }
+
+        fn outcome(self, theirs: Move) -> Outcome {
+            if self.beats(theirs) {
+                Outcome::Win
+            } else if theirs.beats(self) {
+                Outcome::Loss
+            } else {
+                Outcome::Draw
+            }
+        }
+    }
+
+    impl OutcomeTrait for Outcome {
+        fn inherent_points(self) -> usize {
+            match self {
+                Outcome::Win => 6,
+                Outcome::Draw => 3,
+                Outcome::Loss => 0,
+            }
+        }
+
+        fn matching_move(self, theirs: Move) -> Move {
+            match self {
+                Outcome::Win => theirs.winning_move(),
+                Outcome::Draw => theirs.drawing_move(),
+                Outcome::Loss => theirs.losing_move(),
+            }
+        }
+    }
+
+    impl Move {
+        const ALL_MOVES: [Move; 3] = [Move::Rock, Move::Paper, Move::Scissors];
+
+        fn winning_move(self) -> Self {
+            Self::ALL_MOVES
+                .iter()
+                .copied()
+                .find(|m| m.beats(self))
+                .expect("at least one move beats us")
+        }
+
+        fn losing_move(self) -> Self {
+            Self::ALL_MOVES
+                .iter()
+                .copied()
+                .find(|&m| self.beats(m))
+                .expect("we beat at least one move")
+        }
+
+        fn drawing_move(self) -> Self {
+            self
+        }
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    struct Round {
+        theirs: Move,
+        ours: Move,
+    }
+
+    impl Round {
+        fn outcome(self) -> Outcome {
+            self.ours.outcome(self.theirs)
+        }
+
+        fn our_score(self) -> usize {
+            self.ours.inherent_points() + self.outcome().inherent_points()
+        }
+    }
+
+    mod part1 {
+        use super::{Move, Outcome, Round};
+        use std::str::FromStr;
+
+        // Trait wrappers 
+        struct P1Move(Move);
+        struct P1Round(Round);
+
+        // try to parse a Move from either ABC or XYZ
+        impl TryFrom<char> for P1Move {
+            type Error = color_eyre::Report;
+
+            fn try_from(c: char) -> Result<Self, Self::Error> {
+                match c {
+                    'A' | 'X' => Ok(P1Move(Move::Rock)),
+                    'B' | 'Y' => Ok(P1Move(Move::Paper) ),
+                    'C' | 'Z' => Ok(P1Move(Move::Scissors) ),
+                    _ => Err(color_eyre::eyre::eyre!("not a valid move: {c:?}")),
+                }
+            }
+        }
+
+        // parse a Round given a &str (a single line)
+        impl FromStr for P1Round {
+            type Err = color_eyre::Report;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                let mut chars = s.chars();
+                let (Some(theirs), Some(' '), Some(ours), None) =
+                    (chars.next(), chars.next(), chars.next(), chars.next())
+                else {
+                    return Err(color_eyre::eyre::eyre!(
+                        "expected <theirs>SP<ours>EOF, got {s:?}"
+                    ));
+                };
+                {
+                    Ok(P1Round(Round {
+                        theirs: <char as std::convert::Into<P1Move>>::into(theirs)?.0,
+                        ours: ours.try_into()?,
+                    }))
+                }
+            }
+        }
+
+        #[cfg(test)]
+        mod test_main {
+            use super::*;
+            use itertools::{process_results, Itertools};
+
+            #[test]
+            fn main_ok() -> color_eyre::Result<()> {
+                color_eyre::install()?;
+                {
+                    use self::*;
+
+                    let total_score: usize = itertools::process_results(
+                        include_str!("input.txt")
+                            .lines()
+                            .map(Round::from_str)
+                            // 👇 this is provided by `Itertools`
+                            .map_ok(|r| r.our_score()),
+                        |it| it.sum(),
+                    )?;
+                    dbg!(total_score);
+                }
+
+                Ok(())
+            }
+        }
+    }
+
+    mod part2 {
+        use super::{Move, Outcome, Round};
+        use std::str::FromStr;
+
+        impl TryFrom<char> for Move {
+            type Error = color_eyre::Report;
+
+            fn try_from(c: char) -> Result<Self, Self::Error> {
+                match c {
+                    'A' => Ok(Move::Rock),
+                    'B' => Ok(Move::Paper),
+                    'C' => Ok(Move::Scissors),
+                    _ => Err(color_eyre::eyre::eyre!("not a valid move: {c:?}")),
+                }
+            }
+        }
+
+        impl TryFrom<char> for Outcome {
+            type Error = color_eyre::Report;
+
+            fn try_from(c: char) -> Result<Self, Self::Error> {
+                match c {
+                    'X' => Ok(Outcome::Loss),
+                    'Y' => Ok(Outcome::Draw),
+                    'Z' => Ok(Outcome::Win),
+                    _ => Err(color_eyre::eyre::eyre!("not a valid outcome: {c:?}")),
+                }
+            }
+        }
+
+        // a helper to Outcome itself, to find the move that matches, given their move
+        impl Outcome {
+            fn matching_move(self, theirs: Move) -> Move {
+                match self {
+                    Outcome::Win => theirs.winning_move(),
+                    Outcome::Draw => theirs.drawing_move(),
+                    Outcome::Loss => theirs.losing_move(),
+                }
+            }
+        }
+
+        // change Round's FromStr implementation to parse their move, the desired outcome, and decide what our move should be
+        impl FromStr for Round {
+            type Err = color_eyre::Report;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                let mut chars = s.chars();
+                let (Some(theirs), Some(' '), Some(outcome), None) =
+                    (chars.next(), chars.next(), chars.next(), chars.next())
+                else {
+                    return Err(color_eyre::eyre::eyre!(
+                        "expected <theirs>SP<outcome>EOF, got {s:?}"
+                    ));
+                };
+                {
+                    use self::*;
+                    let theirs = Move::try_from(theirs)?;
+                    let outcome = Outcome::try_from(outcome)?;
+                    let ours = outcome.matching_move(theirs);
+                    Ok(Self { theirs, ours })
+                }
+            }
+        }
+
+        #[cfg(test)]
+        mod test_main {
+            use super::*;
+
+            #[test]
+            fn part2_main() {
+                {
+                    use self::*;
+                    let total_score: usize = itertools::process_results(
+                        include_str!("input.txt")
+                            .lines()
+                            .map(Round::from_str)
+                            //    there 👇   👇
+                            .map_ok(|r| dbg!(dbg!(r).our_score())),
+                        |it| it.sum(),
+                    )?;
+                    dbg!(total_score);
+                }
+            }
+        }
+    }
 }
--- day3/src/utils.rs
+++ day3/src/utils.rs
@@ -1,47 +1,46 @@
 #![allow(unused)]
+use std::collections::HashSet;
 use std::fs;
 use std::path::Path;
-use std::collections::HashSet;
 
-pub fn total_priority_sum(input_path: &str) -> u32{
+pub fn total_priority_sum(input_path: &str) -> u32 {
     let lines = read_lines(input_path);
     let mut total = 0;
 
-    for line in lines.iter(){
+    for line in lines.iter() {
         let c = shared_item(line);
         let val = priority_val(c);
-        total+= val;
+        total += val;
     }
-    total 
+    total
 }
 
-pub fn get_badges_sum(file_path: &str)-> u32{
+pub fn get_badges_sum(file_path: &str) -> u32 {
     let lines = read_lines(file_path);
     let length = lines.len();
     let mut badges: Vec<char> = Vec::new();
 
-    let mut i =0;
-    while i != length{
+    let mut i = 0;
+    while i != length {
         let r1 = &lines[i];
-        let r2 = &lines[i+1];
-        let r3 = &lines[i+2];
+        let r2 = &lines[i + 1];
+        let r3 = &lines[i + 2];
 
         let set: HashSet<char> = r1.chars().collect();
         let shared: Vec<char> = r2.chars().filter(|c| set.contains(c)).collect();
         let commons: Vec<char> = r3.chars().filter(|c| shared.contains(c)).collect();
 
         badges.push(commons[0]);
-        i+=3;
+        i += 3;
     }
 
     badges.into_iter().map(|c| priority_val(c)).sum()
 }
 
-fn priority_val(c: char) -> u32{
-    if c.is_ascii_lowercase(){
+fn priority_val(c: char) -> u32 {
+    if c.is_ascii_lowercase() {
         c as u32 - 96
-    }
-    else{
+    } else {
         c as u32 - 38
     }
 }
@@ -53,12 +52,12 @@ fn shared_item(rucksack: &str) -> char {
     let mut set: HashSet<char> = compartments.0.chars().collect();
     let mut shared: Vec<char> = compartments.1.chars().filter(|c| set.contains(c)).collect();
 
-    shared[0] 
+    shared[0]
 }
 
-fn compartments(rucksack: &str) -> (&str,&str){
+fn compartments(rucksack: &str) -> (&str, &str) {
     let length = rucksack.len();
-    rucksack.split_at(length/2)
+    rucksack.split_at(length / 2)
 }
 
 fn read_lines(file_path: &str) -> Vec<String> {
@@ -80,14 +79,14 @@ mod test_utils {
     }
 
     #[test]
-    fn get_compartments_ok(){
+    fn get_compartments_ok() {
         let lines = read_lines(INPUT);
         let line = lines.get(0).unwrap();
         println!("{:?}", compartments(line));
     }
 
     #[test]
-    fn get_shared_item_ok(){
+    fn get_shared_item_ok() {
         let lines = read_lines(INPUT);
         let line = lines.get(0).unwrap();
         let shared = shared_item(line);
@@ -95,7 +94,7 @@ mod test_utils {
     }
 
     #[test]
-    fn get_val_ok(){
+    fn get_val_ok() {
         assert_eq!(1, priority_val('a'));
         assert_eq!(26, priority_val('z'));
         assert_eq!(52, priority_val('Z'));
@@ -103,12 +102,190 @@ mod test_utils {
     }
 
     #[test]
-    fn get_badges_sum_ok(){
+    fn get_badges_sum_ok() {
         assert_eq!(70, get_badges_sum(INPUT));
     }
 
     #[test]
-    fn get_total_ok(){
-        assert_eq!(157,total_priority_sum(INPUT));
+    fn get_total_ok() {
+        assert_eq!(157, total_priority_sum(INPUT));
+    }
+}
+
+mod another_solution {
+
+    mod item {
+        // to represent item in rucksack i.e char => u8 with added gurantee to be char
+        #[repr(transparent)]
+        #[derive(Copy, Clone, PartialEq, Eq, Hash)]
+        pub(crate) struct Item(u8);
+
+        // convert from u8 to Item fallibly
+        impl TryFrom<u8> for Item {
+            type Error = color_eyre::Report;
+
+            fn try_from(value: u8) -> Result<Self, Self::Error> {
+                match value {
+                    b'a'..=b'z' | b'A'..=b'Z' => Ok(Item(value)),
+                    _ => Err(color_eyre::eyre::eyre!(
+                        "{} is not a valid item",
+                        value as char
+                    )),
+                }
+            }
+        }
+
+        impl std::fmt::Debug for Item {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.0 as char)
+            }
+        }
+
+        impl Item {
+            /// get an item's "priority",
+            pub(crate) fn score(self) -> usize {
+                match self {
+                    Item(b'a'..=b'z') => 1 + (self.0 - b'a') as usize,
+                    Item(b'A'..=b'Z') => 27 + (self.0 - b'A') as usize,
+                    _ => unreachable!(),
+                }
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod test_main {
+        use super::*;
+        use item::Item;
+        use itertools::Itertools;
+
+        #[test]
+        fn check_part1() -> color_eyre::Result<()> {
+            let mut total_score = 0;
+
+            for line in include_str!("input.txt").lines() {
+                let (first, second) = line.split_at(line.len() / 2);
+
+                let first_items = first
+                    .bytes()
+                    .map(Item::try_from)
+                    .collect::<Result<Vec<_>, _>>()?;
+
+                let dupe_score = second
+                    .bytes()
+                    .map(Item::try_from)
+                    .find_map(|item| {
+                        item.ok().and_then(|item| {
+                            first_items
+                                .iter()
+                                // the iterator gives us `&Item`, but we want `Item`, and
+                                // it's `Copy`, so we can call `copied()`
+                                .copied()
+                                // `find` gives us an `&Item`, but we want `Item`, so we
+                                // destructure the reference here:
+                                //    👇
+                                .find(|&first_item| first_item == item)
+                        })
+                    })
+                    .expect("there should be exactly one duplicate")
+                    .score();
+                dbg!(dupe_score);
+                total_score += dupe_score;
+            }
+
+            dbg!(total_score);
+            Ok(())
+        }
+
+        #[test]
+        fn check_part2() -> color_eyre::Result<()> {
+            // use im::HashSet;
+            use itertools::Itertools;
+            use std::collections::HashSet;
+
+            let rucksacks = include_str!("input.txt").lines().map(|line| {
+                line.bytes()
+                    .map(Item::try_from)
+                    .collect::<Result<HashSet<_>, _>>()
+            });
+
+            let sum = itertools::process_results(rucksacks, |rs| {
+                rs.tuples()
+                    .map(|(a, b, c)| {
+                        a.iter()
+                            .copied()
+                            .find(|i| b.contains(i) && c.contains(i))
+                            .map(|i| dbg!(i.score()))
+                            .unwrap_or_default()
+                    })
+                    .sum::<usize>()
+            })?;
+            dbg!(sum);
+
+            Ok(())
+        }
+
+        #[test]
+        fn check_part2_chunks() -> color_eyre::Result<()> {
+            use im::HashSet;
+
+            let sum: usize = include_str!("input.txt")
+                .lines()
+                .map(|line| {
+                    line.bytes()
+                        .map(|b| b.try_into().unwrap())
+                        .collect::<HashSet<Item>>()
+                })
+                .chunks(3)
+                .into_iter()
+                .map(|chunks| {
+                    chunks
+                        .reduce(|a, b| a.intersection(b))
+                        .expect("we always have 3 chunks")
+                        .iter()
+                        .next()
+                        .expect("problem statement says there is always one item in common")
+                        .score()
+                })
+                .sum();
+            dbg!(sum);
+
+            Ok(())
+        }
+
+        #[test]
+        fn check_part2_u8_array() {
+            use itertools::Itertools;
+            let sum: usize = include_str!("input.txt")
+                .lines()
+                .map(|line| {
+                    line.bytes().map(|b| b.try_into().unwrap()).fold(
+                        [0u8; 53],
+                        |mut acc, x: Item| {
+                            // this might panic!
+                            acc[x.score()] = 1;
+                            acc
+                        },
+                    )
+                })
+                .chunks(3)
+                .into_iter()
+                .map(|chunks| {
+                    chunks
+                        .reduce(|mut a, b| {
+                            // another trick: we're re-using `a` as the output array
+                            for (a, b) in a.iter_mut().zip(b.iter()) {
+                                *a += *b;
+                            }
+                            a
+                        })
+                        .expect("we always have 3 chunks")
+                        .iter()
+                        .position(|&b| b == 3)
+                        .expect("problem statement says there is always one item in common")
+                })
+                .sum();
+            dbg!(sum);
+        }
     }
 }
